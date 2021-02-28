81ea76ac17e401eb7bdbed97510c7000e58bf88d
abondar24 <abondar1992@gmail.com>
2021-02-28 12:36:07 +0100

    revert changes

--- src/main/java/org/abondar/spring/ratelimitter/RateLimit.java
+++ src/main/java/org/abondar/spring/ratelimitter/RateLimit.java
@@ -7,7 +7,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Documented
-@Retention(RetentionPolicy.SOURCE)
+@Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD,ElementType.TYPE})
 public @interface RateLimit {
 
